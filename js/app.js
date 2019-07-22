9022ee35ad13986bfdd01b3f80bd6f9bae699fa4
Bulba, Viacheslav <Viacheslav.Bulba@mastercard.com>
2019-07-22 13:13:36 -0500

    remade es5 code to es6 classes

--- js/app.js
+++ js/app.js
@@ -1,124 +1,127 @@
 const canvasWidth = 505;
-const canvasHeight = 606;
 const blockWidth = 101;
 const blockHeight = 83;
 const playerImageYOffset = 10;
 const enemyImageYOffset = 20;
-
 const collisionXOffset = 80;
 const collisionYOffset = 60;
 
-
-
+const playerBottomYPosition = blockHeight*5-playerImageYOffset;
 
 function getRandomSpeed(){
     return 100 + Math.floor(Math.random() * 400);//min speed = 100, max speed = 500
 }
 
 // Enemies our player must avoid
-const Enemy = function(y, speed = getRandomSpeed()) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    this.x = -blockWidth;
-    this.y = y;
-    this.speed = speed;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+class Enemy {
+    constructor(y, speed = getRandomSpeed()) {
+        // Variables applied to each of our instances go here,
+        // we've provided one for you to get started
+        this.x = -blockWidth;
+        this.y = y;
+        this.speed = speed;
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed * dt;
-    if (this.x > canvasWidth) {
-        this.x = -blockWidth;
-        this.speed = getRandomSpeed();
+    update(dt) {
+        this.x += this.speed * dt;
+        if (this.x > canvasWidth) {
+            this.x = -blockWidth;
+            this.speed = getRandomSpeed();
+        }
+        //check if enemy and player collision happens
+        if (this.x + collisionXOffset > player.x &&
+            this.x - collisionXOffset < player.x &&
+            this.y + collisionYOffset > player.y &&
+            this.y - collisionYOffset < player.y
+        ) {
+            player.resetPosition();
+        }
     }
-    //check if enemy and player collision happens
-    if (this.x + collisionXOffset > player.x &&
-        this.x - collisionXOffset < player.x &&
-        this.y + collisionYOffset > player.y &&
-        this.y - collisionYOffset < player.y
-    ) {
-        player.resetPosition();
+
+    // Draw the enemy on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
-};
+}
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+//-------------------------------------------------------
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+class Player {
+    constructor(x = blockWidth * 2, y = playerBottomYPosition) {
+        this.x = x;
+        this.y = y;
+        this.sprite = 'images/char-boy.png';//initial skin for the player is the char boy
+    };
 
-const Player = function (x = blockWidth * 2, y = blockHeight*5-playerImageYOffset){
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/char-boy.png';//initial skin for the player is the char boy
+    resetPosition() {
+        this.x = blockWidth * 2;
+        this.y = playerBottomYPosition;
+    };
+
+    setRandomSkin() {
+        const random = 1 + Math.round(Math.random() * 4);
+        switch (random) {
+            case 1:
+                this.sprite = 'images/char-boy.png';
+                break;
+            case 2:
+                this.sprite = 'images/char-cat-girl.png';
+                break;
+            case 3:
+                this.sprite = 'images/char-horn-girl.png';
+                break;
+            case 4:
+                this.sprite = 'images/char-pink-girl.png';
+                break;
+            case 5:
+                this.sprite = 'images/char-princess-girl.png';
+                break;
+        }
+    };
+
+    //handles for key presses (left, right, up, down)
+    handleInput(key) {
+        if (this.y < 0) { //if player has already reached the water, do nothing waiting to setTimeout will reset it
+            return;
+        }
+        switch (key) {
+            case 'left':
+                this.x > 0 ? this.x -= blockWidth : this.x -= 0;
+                break;
+            case 'right':
+                this.x < canvasWidth - blockWidth ? this.x += blockWidth : this.x += 0;
+                break;
+            case 'up':
+                this.y > 0 ? this.y -= blockHeight : this.y -= 0;
+                break;
+            case 'down':
+                this.y < blockHeight*5-10 ? this.y += blockHeight : this.y += 0;
+                break;
+        }
+        if (this.y < 0) {
+            setTimeout(function (){
+                player.resetPosition();
+                player.setRandomSkin();
+            }, 600);
+        }
+    };
+
+    // Draw the player on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 }
 
 
-Player.prototype.resetPosition = function() {
-    this.x = blockWidth * 2;
-    this.y = blockHeight*5-playerImageYOffset;
-};
 
-Player.prototype.setRandomSkin = function() {
-    const random = 1 + Math.round(Math.random() * 4);
-    switch (random) {
-        case 1:
-            this.sprite = 'images/char-boy.png';
-            break;
-        case 2:
-            this.sprite = 'images/char-cat-girl.png';
-            break;
-        case 3:
-            this.sprite = 'images/char-horn-girl.png';
-            break;
-        case 4:
-            this.sprite = 'images/char-pink-girl.png';
-            break;
-        case 5:
-            this.sprite = 'images/char-princess-girl.png';
-            break;
-    }
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.handleInput = function(key) {
-    if (this.y < 0) { //if player has already reached the water, do nothing waiting to setTimeout will reset it
-        return;
-    }
-    switch (key) {
-        case 'left':
-            this.x > 0 ? this.x -= blockWidth : this.x -= 0;
-            break;
-        case 'right':
-            this.x < canvasWidth - blockWidth ? this.x += blockWidth : this.x += 0;
-            break;
-        case 'up':
-            this.y > 0 ? this.y -= blockHeight : this.y -= 0;
-            break;
-        case 'down':
-            this.y < blockHeight*5-10 ? this.y += blockHeight : this.y += 0;
-            break;
-    }
-    if (this.y < 0) {
-        setTimeout(function (){
-            player.resetPosition();
-            player.setRandomSkin();
-        }, 600);
-    }
-};
 
 
 // Now instantiate your objects.
